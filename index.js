abb98b4bf1dcb2ed138b1d659710df95415b4b2a
Matthew <matthew@stateofmind.agency>
2020-02-06 22:55:49 +0000

    Refactored creation of images file with extname.

--- index.js
+++ index.js
@@ -10,19 +10,13 @@ fs.mkdir(`${desktop}/cleanup`, () => {
     console.log('Cleanup file created. 🧼')
     // get all files located in the desktop
     fs.readdir(desktop, (error, files) => {
-        // create an array with each file in the desktop
-        const fileArray = []
         files.forEach(file => {
-            fileArray.push(file)
-        })
-        fileArray.forEach(file => {
-            console.log(file)
-            let item = file.split('.')
-            if(item[item.length - 1] === 'png' || 'jpg') {
-                fs.mkdir(`${desktop}/cleanup/images`, () => {
-                    // console.log('Image file created 📸')
-                })
-            }
+            // check if the extension of each file is an image
+            path.extname(`${desktop}/${file}`) === '.png'
+            // create an image folder 
+            && fs.mkdir(`${desktop}/cleanup/images`, () => {
+                console.log(`Created an image folder. 📸`)
+            })
         })
     })
 })
